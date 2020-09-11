207ccecff3ef13cbf790a33f456f7fec1e53d0e5
Abhinav <xabhi7549@gmail.com>
2020-09-11 14:34:08 +0530

    Initial commit

---
+++ app/src/main/java/com/site_valley/www/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) $today.year.All rights reserved.
+ *  Last modified 11/9/20 2:30 PM
+ */
+
+package com.site_valley.www.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
